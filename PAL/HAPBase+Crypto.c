420598d9eeff6c3c90ac2dc9f7abb5cd88ce74cf
terafin <jwood@me.com>
2020-02-27 14:29:27 -0800

    Update HAPBase+Crypto.c

--- PAL/HAPBase+Crypto.c
+++ PAL/HAPBase+Crypto.c
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the “License”);
 // you may not use this file except in compliance with the License.
-// See [CONTRIBUTORS.md] for the list of HomeKit ADK project authors.
+// See [CONTRIBUTORS.md] for   the list of HomeKit ADK project authors.
 
 #include "HAPBase.h"
 #include "HAPCrypto.h"
