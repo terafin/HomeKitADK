982fe1fbdc36f9ed24634c7de1880166e174118f
Justin Wood <jwood@me.com>
2020-02-27 12:24:24 -0800

    Next

--- Actions/Clang/run_clang.sh
+++ Actions/Clang/run_clang.sh
@@ -4,18 +4,20 @@ cd $GITHUB_WORKSPACE
 clang-tidy HAP/*.c  -- -IHAP -IPAL -IExternal/HTTP -IExternal/JSON -IExternal/Base64 > clang_output.txt
 
 CLANG_OUTPUT=`cat clang_output.txt`
-COMMENTS_URL=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.comments_url)
-  
-echo $COMMENTS_URL
-echo "Clang errors:"
+echo "Clang output:"
 echo $CLANG_OUTPUT
-
-echo "Event path: $GITHUB_EVENT_PATH"
-cat $GITHUB_EVENT_PATH
 
 OUTPUT=$'**CLANG WARNINGS**:\n'
 OUTPUT+=$'\n```\n'
 
-PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
+echo "Event path: $GITHUB_EVENT_PATH"
+cat $GITHUB_EVENT_PATH
 
-curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.VERSION.text+json" --data "$PAYLOAD" "$COMMENTS_URL"
+COMMENTS_URL=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.comments_url)
+  
+echo "Comment URL: $COMMENTS_URL"
+
+if [ -n "$COMMENTS_URL" ]; then #check to see if we're a pull request
+    PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
+    curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.VERSION.text+json" --data "$PAYLOAD" "$COMMENTS_URL"
+fi
