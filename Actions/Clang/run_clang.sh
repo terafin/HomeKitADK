aeaae773015d6471f468b968d8061655d380ecc4
Justin Wood <jwood@me.com>
2020-02-27 15:59:38 -0800

    I think this is working?

--- Actions/Clang/run_clang.sh
+++ Actions/Clang/run_clang.sh
@@ -2,34 +2,46 @@
 
 cd $GITHUB_WORKSPACE
 
-
-
-# clang-tidy HAP/*.c PAL/*.c PAL/Mock/*.c External/HTTP/*.c External/JSON/*.c External/Base64/*.c -- -IHAP -IPAL -IExternal/HTTP -IExternal/JSON -IExternal/Base64 > clang_output.txt
+# CLANG_ARGUMENTS="-- -IHAP -IPAL -IExternal/HTTP -IExternal/JSON -IExternal/Base64"
+# FILES_MODIFIED="HAP/HAPIPAccessoryProtocol.c HAP/HAPMACAddress.c"
 
 echo "clang_arguments: $CLANG_ARGUMENTS"
 echo "files-modified: $FILES_MODIFIED"
 echo "files-added: $FILES_ADDED"
 
-IFS=' ' read -r -a MODIFIED_FILES <<< "$FILES_MODIFIED"
-IFS=' ' read -r -a ADDED_FILES <<< "$FILES_ADDED"
+read -r -a MODIFIED_FILES <<< "$FILES_MODIFIED"
+read -r -a ADDED_FILES <<< "$FILES_ADDED"
 
 clang_arguments=""
-for element in "${array[@]}"
+for element in "${MODIFIED_FILES[@]}"
+do
+    clang_arguments+="$element "
+done
+for element in "${ADDED_FILES[@]}"
 do
     clang_arguments+="$element "
 done
 clang_arguments+="$CLANG_ARGUMENTS "
 
-clang-tidy "$clang_arguments" > clang_output.txt
+clang_arguments+=" -export-fixes=fixes.yml"
+
+echo "clang arguments: $clang_arguments"
+
+clang-tidy $clang_arguments > clang_output.txt
 
 CLANG_OUTPUT=`cat clang_output.txt`
-echo "Clang output:"
+FIXES_OUTPUT=`cat fixes.yml`
+rm clang_output.txt
+rm fixes.yml
+
+echo "Warnings / Errors output:"
 echo $CLANG_OUTPUT
 
 PULL_REQUEST_COMMENT=""
 PULL_REQUEST_COMMENT+=$'\n```\n'
 PULL_REQUEST_COMMENT+=$'Clang output:\n'
 PULL_REQUEST_COMMENT+="$CLANG_OUTPUT"
+PULL_REQUEST_COMMENT+="$FIXES_OUTPUT"
 PULL_REQUEST_COMMENTz+=$'\n```\n'
 
 echo "Event path: $GITHUB_EVENT_PATH"
