619d4cae64d36eefde87a9e1a23f4f5b8abbb053
Justin Wood <jwood@me.com>
2020-02-27 16:13:16 -0800

    Cleaner comment

--- Actions/Clang/run_clang.sh
+++ Actions/Clang/run_clang.sh
@@ -2,12 +2,9 @@
 
 cd $GITHUB_WORKSPACE
 
-# CLANG_ARGUMENTS="-- -IHAP -IPAL -IExternal/HTTP -IExternal/JSON -IExternal/Base64"
-# FILES_MODIFIED="HAP/HAPIPAccessoryProtocol.c HAP/HAPMACAddress.c"
-
-echo "clang_arguments: $CLANG_ARGUMENTS"
-echo "files-modified: $FILES_MODIFIED"
-echo "files-added: $FILES_ADDED"
+echo "clang arguments: $CLANG_ARGUMENTS"
+echo "files modified: $FILES_MODIFIED"
+echo "files added: $FILES_ADDED"
 
 read -r -a MODIFIED_FILES <<< "$FILES_MODIFIED"
 read -r -a ADDED_FILES <<< "$FILES_ADDED"
@@ -27,32 +24,32 @@ clang_arguments+=" -export-fixes=fixes.yml"
 clang_arguments+=" $CLANG_ARGUMENTS "
 
 
-echo "clang arguments: $clang_arguments"
+echo "final clang arguments: $clang_arguments"
 
+# Run clang-tidy for real
 clang-tidy $clang_arguments > clang_output.txt
 
 CLANG_OUTPUT=`cat clang_output.txt`
 FIXES_OUTPUT=`cat fixes.yml`
+LINES_OF_FIXES=$(echo $FIXES_OUTPUT | wc -l)
+
+# Cleanup the files we don't need
 rm clang_output.txt
 rm fixes.yml
 
-echo "Warnings / Errors output:"
-echo $CLANG_OUTPUT
-
-PULL_REQUEST_COMMENT=""
+PULL_REQUEST_COMMENT="### Automated clang-tidy Review\n"
+PULL_REQUEST_COMMENT="#### Clang Warnings & Errors\n"
 PULL_REQUEST_COMMENT+=$'\n```\n'
-PULL_REQUEST_COMMENT+=$'Clang Warnings & Errors:\n'
 PULL_REQUEST_COMMENT+="$CLANG_OUTPUT"
+PULL_REQUEST_COMMENTz+=$'\n```\n'
+
+PULL_REQUEST_COMMENT="#### Recommended Fixes\n"
+PULL_REQUEST_COMMENTz+=$'\n```\n'
 PULL_REQUEST_COMMENT+="$FIXES_OUTPUT"
 PULL_REQUEST_COMMENTz+=$'\n```\n'
 
-echo "Event path: $GITHUB_EVENT_PATH"
-cat $GITHUB_EVENT_PATH
-
 PULL_REQUEST_COMMENT_URL=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.comments_url)
   
-echo "Comment URL: $PULL_REQUEST_COMMENT_URL"
-
 echo ::set-output name=clang-result::$CLANG_OUTPUT
 
 if [ ! -z "$PULL_REQUEST_COMMENT_URL" -a "$str"!="" -a "$str"!="null" ]; then
