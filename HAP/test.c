c18eb10ed1f074b83e30c2db57889dae17dda138
terafin <jwood@me.com>
2020-02-28 09:25:35 -0800

    Update test.c

--- HAP/test.c
+++ HAP/test.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-int main() {
-   int value = 2;
-   
+
+int main(int argc, char **argv) {
+   char * value = "baob";
+
+    value = "baoba";
+    
    if ( 0 ) {
       printf("testing: %s", "hello?");
    }
    printf("Helqweo, World!");
+
    return 0;
 }
+
